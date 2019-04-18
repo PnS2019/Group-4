c5ad1978afbdf03684e6740ed30f32cd95a2fbd8
kevin <kevin.mondoloni.ethz@gmail.com>
2019-04-18 16:07:46 +0200

    face_detect, takePicture with picamera

--- Project/face_detect.py
+++ Project/face_detect.py
@@ -5,10 +5,17 @@ from pnslib import utils
 def get_face(img):
     face_cascade = cv2.CascadeClassifier(
         utils.get_haarcascade_path('haarcascade_frontalface_default.xml'))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
+    cv2.imshow('original image', img)
+        
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=5)
     (x, y, w, h) = faces[0]
     face = img[y:y+h, x:x+w]
+    
     face = cv2.resize(face, (80, 80), interpolation=cv2.INTER_CUBIC)
-    #face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow('face', face)
+    
     return face
