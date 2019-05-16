bf4a7479b197e9d0f9181ee3767adde952a4c0ad
xiaorui <yinx@student.ethz.ch>
2019-05-16 12:41:46 +0200

    delete old file

--- Project/prediction.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-import numpy as np
-import cv2
-
-model = load_model("facial_expression_detection.df5")
-
-def predict(img):
-    pred = model.predict(img)
-    print(pred)
